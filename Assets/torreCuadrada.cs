a9a0b757bfd7685d4d7e805a506dc2fdb064419c
yulian.lyubomirov <yulian.lyubomirov@alumnos.upm.es>
2023-05-01 16:20:36 +0200

    Torre cuadrada arreglada

--- Assets/torreCuadrada.cs
+++ Assets/torreCuadrada.cs
@@ -16,11 +16,12 @@ public class torreCuadrada : MonoBehaviour
         colorList.Add(Color.yellow);
         colorList.Add(Color.magenta);
      Vector3 posicionTorre = GetComponent<Rigidbody>().transform.position;
-     for (int y = 0; y < 21; y++) {
-        if(y%2==0){
+    for (int y = 1; y < 21; y++) {
+        if(y%2==1){
             offset=0;
+            
             for (int x = 0; x < 6; x++) {
-                GameObject ladrillo1 = Instantiate(ladrillo, new Vector3(x+offset,y,0)+posicionTorre, Quaternion.identity);
+                GameObject ladrillo1 = Instantiate(ladrillo, new Vector3(offset,y,-0.5f)+posicionTorre, Quaternion.identity);
                 Renderer brickRenderer = ladrillo1.GetComponent<Renderer>();
                 if (brickRenderer != null)
                 {
@@ -28,34 +29,91 @@ public class torreCuadrada : MonoBehaviour
                     Color randomColor = colorList[Random.Range(0, colorList.Count)];
                     brickRenderer.material.color = randomColor;
                 }
-                Instantiate(ladrillo, new Vector3(x+offset,y,12)+posicionTorre, Quaternion.identity);
-                offset=offset+1;
+                ladrillo1 = Instantiate(ladrillo, new Vector3(offset,y,10.5f)+posicionTorre, Quaternion.identity);
+                Renderer brickRenderer2 = ladrillo1.GetComponent<Renderer>();
+               if (brickRenderer2 != null)
+                {
+                    // Choose a random color from the list and set the brick's material to that color
+                    Color randomColor = colorList[Random.Range(0, colorList.Count)];
+                    brickRenderer2.material.color = randomColor;
+                }
+                offset=offset+2;
             }
             offset=1;
+          
             for (int x=0;x<5; x++){
-                Instantiate(ladrillo, new Vector3(0,y,x+offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
-                Instantiate(ladrillo, new Vector3(12,y,x+offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
-                offset=offset+1;
+                GameObject ladrillo1 = Instantiate(ladrillo, new Vector3(-0.5f,y,offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
+                Renderer brickRenderer = ladrillo1.GetComponent<Renderer>();
+                if (brickRenderer != null)
+                {
+                    // Choose a random color from the list and set the brick's material to that color
+                    Color randomColor = colorList[Random.Range(0, colorList.Count)];
+                    brickRenderer.material.color = randomColor;
+                }
+                ladrillo1= Instantiate(ladrillo, new Vector3(10.5f,y,offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
+                Renderer brickRenderer2 = ladrillo1.GetComponent<Renderer>();
+              if (brickRenderer2 != null)
+                {
+                    // Choose a random color from the list and set the brick's material to that color
+                    Color randomColor = colorList[Random.Range(0, colorList.Count)];
+                    brickRenderer2.material.color = randomColor;
+                }
+                offset=offset+2;
             }
         }
         else{
             offset=1;
+            
             for (int x = 0; x < 5; x++) {
-                Instantiate(ladrillo, new Vector3(x+offset,y,0)+posicionTorre, Quaternion.identity);
-                Instantiate(ladrillo, new Vector3(x+offset,y,12)+posicionTorre, Quaternion.identity);
-                offset=offset+1;
+               GameObject ladrillo1=  Instantiate(ladrillo, new Vector3(offset,y,-0.5f)+posicionTorre, Quaternion.identity);
+               Renderer brickRenderer = ladrillo1.GetComponent<Renderer>();
+                if (brickRenderer != null)
+                {
+                    // Choose a random color from the list and set the brick's material to that color
+                    Color randomColor = colorList[Random.Range(0, colorList.Count)];
+                    brickRenderer.material.color = randomColor;
+                }
+                ladrillo1= Instantiate(ladrillo, new Vector3(offset,y,10.5f)+posicionTorre, Quaternion.identity);
+                Renderer brickRenderer2 = ladrillo1.GetComponent<Renderer>();
+              if (brickRenderer2 != null)
+                {
+                    // Choose a random color from the list and set the brick's material to that color
+                    Color randomColor = colorList[Random.Range(0, colorList.Count)];
+                    brickRenderer2.material.color = randomColor;
+                }
+                offset=offset+2;
+
             }
             offset=0;
+            
             for (int x=0;x<6; x++){
-                Instantiate(ladrillo, new Vector3(0,y,x+offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
-                Instantiate(ladrillo, new Vector3(12,y,x+offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
-                 offset=offset+1;
+                GameObject ladrillo1 = Instantiate(ladrillo, new Vector3(-0.5f,y,offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
+                Renderer brickRenderer = ladrillo1.GetComponent<Renderer>();
+                if (brickRenderer != null)
+                {
+                    // Choose a random color from the list and set the brick's material to that color
+                    Color randomColor = colorList[Random.Range(0, colorList.Count)];
+                    brickRenderer.material.color = randomColor;
+                }
+                ladrillo1= Instantiate(ladrillo, new Vector3(10.5f,y,offset)+posicionTorre,Quaternion.Euler(0f, 90f, 0f));
+                Renderer brickRenderer2 = ladrillo1.GetComponent<Renderer>();
+              if (brickRenderer2 != null)
+                {
+                    // Choose a random color from the list and set the brick's material to that color
+                    Color randomColor = colorList[Random.Range(0, colorList.Count)];
+                    brickRenderer2.material.color = randomColor;
+                }
+                 offset=offset+2;
             }
         }
         
         }
+ }
+    
         
-    }
+
+        
+    
 
     // Update is called once per frame
     void Update()
